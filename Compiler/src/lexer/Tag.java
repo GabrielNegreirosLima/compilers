26f4a8e177abd8a0f87380ae3537135cd46cdba1
Gabriel Negreiros Lima <gabrielnegreiroslima@gmail.com>
2021-01-30 17:56:45 -0300

    Add á¹ professor Kecia code, based in Aho's Compilers textbook.

---
+++ Compiler/src/lexer/Tag.java
@@ -0,0 +1,30 @@
+public class Tag {
+    public final static int
+        
+			//Palavras reservadas
+        PRG = 256,
+        BEG = 257,
+        END = 258,
+        TYPE = 259,
+        INT = 260,
+        CHAR = 261,
+        BOOL = 262,
+        
+		//Operadores e pontuacao
+        // ...
+        EQ = 288,
+        GE = 289,
+        LE = 290,
+        NE = 291,
+		AND = 292,
+		OR = 293,
+		TRUE = 294,
+		FALSE = 295, 
+		IF = 296,
+        
+		// ...
+        //Outros tokens
+        NUM = 278,
+        ID = 279;
+}
+
---
+++ Compiler/src/lexer/Token.java
@@ -0,0 +1,12 @@
+public class Token{
+	public final int tag; 
+	
+	//constante que representa o token
+	public Token(int t){
+		tag= t;
+	}
+	
+	public String toString(){
+		return "" + tag;
+	}
+}
