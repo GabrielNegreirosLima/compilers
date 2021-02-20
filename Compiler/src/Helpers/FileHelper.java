045bc0224e6afd84b88be9621b1a0b3e65087d9f
Yan Victor <yanvictorsa@yahoo.com.br>
2021-02-19 23:04:43 -0300

    Fix file reader

--- Compiler/src/Helpers/FileHelper.java
+++ Compiler/src/Helpers/FileHelper.java
@@ -15,16 +15,25 @@ public class FileHelper {
         else{
             divisor = "/";
         }
-        String path = divisor + "Compiler" + divisor + "src" + divisor + "compiler" + divisor + "Tests";
+        String path = divisor + "src" + divisor + "compiler" + divisor + "Tests";
+        
+        System.out.println(path);
 
         String fullPath = directory.getAbsolutePath() + path;
+        
+        System.out.println(fullPath);
+        
         File[] listFiles = new File(fullPath).listFiles();
         
+        System.out.println(listFiles);
+             
         ArrayList<String> files = new ArrayList<String>();
-
+        
         for (File file : listFiles) {
+            System.out.println("passei auqi 3");
             if (file.isFile()) {
                 files.add(fullPath + divisor + file.getName());
+                System.out.println("passei auqi");
             }
         }
 
